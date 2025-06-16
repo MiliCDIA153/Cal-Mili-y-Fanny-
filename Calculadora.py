45604e5b49de1be7ceac778e3bdb812dd9905d74
MiliCDIA153 <ebautista1024@pue.conalep.edu.mx>
2025-06-16 09:33:05 -0600

    Calculadora.py

--- Calculadora.py
+++ Calculadora.py
@@ -1,1 +1,5 @@
-# Cal-Mili-y-Fanny-
+print("suma")
+num1=int(input("escribe el primer numero:"))
+num2=int(input("escribe el segundo número:"))
+sum=num1+num2
+print("la suma de los dos numeros es:",sum)
