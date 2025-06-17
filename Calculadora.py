607bf8e6030dba417d8a293ac707bcbbb80822ff
MiliCDIA153 <ebautista1024@pue.conalep.edu.mx>
2025-06-17 10:25:29 -0600

    Update Calculadora.py

--- Calculadora.py
+++ Calculadora.py
@@ -3,3 +3,8 @@ num1=int(input("escribe el primer numero:"))
 num2=int(input("escribe el segundo número:"))
 sum=num1+num2
 print("la suma de los dos numeros es:",sum)
+print("resta")
+n1=int(input("escribe el primer numero"))
+n2=int(input("escrbe el segundo numero"))
+res=n1-n2
+print("la resta de los dos numeros es:",res)
